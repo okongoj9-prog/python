4474aa50067a940a3ba4926ed0e8961249333a4e
okongoj9-prog <okongoj9@gmail.com>
2026-06-15 04:28:22 -0700

    added lesson 1b

---
+++ lesson1b.py
@@ -0,0 +1,18 @@
+# python data types
+# when we about data type , we mean the content hold inside of variable
+# Interger = indicates a whole number is python. Note , when indicate an integer , we don't use
+# quation marks
+
+age = 36
+print(age)
+print(type(age))
+
+#float=is number that has a decimal
+pi =3.142
+print(pi)
+print(type(pi))
+
+# String = This is a series of charachters that are enclosed inside of quatation marks
+name = "mary"
+print(name)
+print(type(name))
